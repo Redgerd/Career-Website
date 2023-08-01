948f9aa204b70bf81e11bce606082784cd480edf
Redgerd <117646793+Redgerd@users.noreply.github.com>
2023-08-01 12:59:18 +0000

    Render Dynamic data from python lis into html home

--- app.py
+++ app.py
@@ -2,9 +2,26 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS = [
+  {
+  'id':1,
+  'title' : 'Data Scientist',
+  'Salary' : 'Rs. 12,000,000'
+  },
+  {
+  'id':2,
+  'title' : 'Data Analyst',
+  'Salary' : 'Rs. 8,000,000'
+  },
+{
+  'id':3,
+  'title' : 'Data Engineer',
+  'Salary' : 'Rs. 15,000,000'
+  }
+]
 @app.route("/")
-def hellow_world():
-  return render_template('home.html')
+def hellow_redgerd():
+  return render_template('home.html',jobs=JOBS,company_name='Redgerd')
 
 print(__name__)
 if __name__ == "__main__":
