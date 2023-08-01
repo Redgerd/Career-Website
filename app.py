f27a0e45ab1137e420329f8a566c631412522b85
Redgerd <117646793+Redgerd@users.noreply.github.com>
2023-08-01 11:11:08 +0000

    HTML layout

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask 
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def hellow_world():
-  return "Hellow World"
+  return render_template('home.html')
 
 print(__name__)
 if __name__ == "__main__":
